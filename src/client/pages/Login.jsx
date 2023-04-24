94e9f3c46fc3be0177527989c7df38c55da6cda8
Slava Melikov <samuslav@gmail.com>
2023-04-24 16:36:30 -0400

    fixed homepage

--- src/client/pages/Login.jsx
+++ src/client/pages/Login.jsx
@@ -1,130 +1,127 @@
-import React from "react";
+import React from 'react';
 import {
-    Box,
-    Button,
-    Card,
-    CardContent,
-    TextField,
-    Typography,
-} from "@mui/material";
+  Box,
+  Button,
+  Card,
+  CardContent,
+  TextField,
+  Typography,
+} from '@mui/material';
 
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
-import { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 const styles = {
-    root: {
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-        // backgroundImage: `url(${signupbg})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        overflow: "hidden",
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+    // backgroundImage: `url(${signupbg})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    overflow: 'hidden',
+  },
+  card: {
+    backgroundColor: 'transparent',
+    backdropFilter: 'blur(24px) brightness(125%)',
+    borderRadius: '8px',
+    maxWidth: '400px',
+    width: '100%',
+    boxShadow: '0px 2px 8px rgba(0, 0, 0, 0.1)',
+    padding: '16px',
+  },
+  input: {
+    marginBottom: '16px',
+  },
+  submitButton: {
+    marginTop: '16px',
+    marginBottom: '8px',
+    backgroundColor: '#227BA5',
+    color: '#ffffff',
+    '&:hover': {
+      backgroundColor: '#1D6490',
     },
-    card: {
-        backgroundColor: "transparent",
-        backdropFilter: "blur(24px) brightness(125%)",
-        borderRadius: "8px",
-        maxWidth: "400px",
-        width: "100%",
-        boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.1)",
-        padding: "16px",
+  },
+  signupLink: {
+    color: '##227BA5',
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'underline',
     },
-    input: {
-        marginBottom: "16px",
-    },
-    submitButton: {
-        marginTop: "16px",
-        marginBottom: "8px",
-        backgroundColor: "#227BA5",
-        color: "#ffffff",
-        "&:hover": {
-            backgroundColor: "#1D6490",
-        },
-    },
-    signupLink: {
-        color: "##227BA5",
-        textDecoration: "none",
-        "&:hover": {
-            textDecoration: "underline",
-        },
-        cursor: "pointer",
-    },
+    cursor: 'pointer',
+  },
 };
 const Login = ({ setUser }) => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
-    const loginHandler = () => {
-        if (!email || !password) {
-            return;
-        }
-        axios
-            .post("http://localhost:3001/user/login", { email, password })
-            .then((response) => {
-                console.log(response.data);
-                localStorage.setItem("isLoggedIn", "true");
-                localStorage.setItem("userId", `${response.data._id}`);
-                setUser(response.data);
-                navigate("/");
-            })
-            .catch((e) => console.log("oopsie"));
-    };
-    return (
-        <Box sx={styles.root}>
-            <Card sx={styles.card}>
-                <CardContent>
-                    <Typography
-                        variant="h5"
-                        sx={{
-                            mb: "16px",
-                            textAlign: "center",
-                        }}
-                    >
-                        Log In
-                    </Typography>
-                    <TextField
-                        sx={styles.input}
-                        label="Email"
-                        variant="outlined"
-                        size="small"
-                        fullWidth
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <TextField
-                        sx={styles.input}
-                        label="Password"
-                        variant="outlined"
-                        size="small"
-                        type="password"
-                        fullWidth
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <Button
-                        sx={styles.submitButton}
-                        variant="contained"
-                        size="large"
-                        fullWidth
-                        onClick={loginHandler}
-                    >
-                        Login
-                    </Button>
-                    <Typography
-                        variant="body2"
-                        sx={{ textAlign: "center", mt: "16px" }}
-                    >
-                        Don't have an account?{" "}
-                        <a href="/signup" sx={styles.signupLink}>
-                            Sign up
-                        </a>
-                    </Typography>
-                </CardContent>
-            </Card>
-        </Box>
-    );
+  const loginHandler = () => {
+    if (!email || !password) {
+      return;
+    }
+    axios
+      .post('http://localhost:3001/user/login', { email, password })
+      .then((response) => {
+        console.log(response.data);
+        localStorage.setItem('isLoggedIn', 'true');
+        localStorage.setItem('userId', `${response.data._id}`);
+        setUser(response.data);
+        navigate('/home');
+      })
+      .catch((e) => console.log('oopsie'));
+  };
+  return (
+    <Box sx={styles.root}>
+      <Card sx={styles.card}>
+        <CardContent>
+          <Typography
+            variant='h5'
+            sx={{
+              mb: '16px',
+              textAlign: 'center',
+            }}
+          >
+            Log In
+          </Typography>
+          <TextField
+            sx={styles.input}
+            label='Email'
+            variant='outlined'
+            size='small'
+            fullWidth
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            sx={styles.input}
+            label='Password'
+            variant='outlined'
+            size='small'
+            type='password'
+            fullWidth
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            sx={styles.submitButton}
+            variant='contained'
+            size='large'
+            fullWidth
+            onClick={loginHandler}
+          >
+            Login
+          </Button>
+          <Typography variant='body2' sx={{ textAlign: 'center', mt: '16px' }}>
+            Don't have an account?{' '}
+            <a href='/signup' sx={styles.signupLink}>
+              Sign up
+            </a>
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
+  );
 };
 
 export default Login;
