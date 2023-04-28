91d42880242e0da60777c681c26247d8fa9416f6
Sidney Brodsky <sidneybrodsky@sidneys-air.mynetworksettings.com>
2023-04-28 10:07:47 -0400

    adds alert boilerplate

--- src/server/controllers/alertController.js
+++ src/server/controllers/alertController.js
@@ -1,5 +1,8 @@
 const axios = require('axios');
-const Alert = require('../models/alertModel');
+const Alert = require('../models/alertModel'); 
+const User = require('../models/userModel'); 
+const ZAPIER_HOOK_URL = 'https://hooks.zapier.com/hooks/catch/15123291/3uedfdp/';
+
 
 const alertController = {
   async getUserAlerts(req, res, next) {
@@ -38,13 +41,42 @@ const alertController = {
     });
     return next();
   },
-  checkMetrics(req, res, next) {
-    const { id } = res.body;
-    const metrics = res.locals.metric;
-    if (!metrics) {
-      return next({ message: 'no metrics found' });
-    }
-  },
+	async checkRanges(req, res, next) {
+		const {userId} = req.body; 
+		const metrics = res.locals.metric;
+		let outOfRangeMessage = '';
+	
+		try {
+			// Fetch the user object using the user ID
+			const user = await User.findById(userId);
+	
+			// Fetch the alerts associated with the user
+			const userAlerts = await Alert.find({ owner: userId });
+	
+			// Iterate through the user alerts and check if any metric is out of range
+			for (const alert of userAlerts) {
+				const metricKey = alert.metric;
+				const metricValue = metrics[metricKey];
+	
+				if (metricValue < alert.under || metricValue > alert.over) {
+					outOfRangeMessage += `Metric "${metricKey}" is out of range with a value of ${metricValue}. `;
+				}
+			}
+	
+			if (outOfRangeMessage) {
+				res.locals.outOfRangeMessage = outOfRangeMessage;
+				const phoneNumber = user.phoneNumber;
+				await axios.post(ZAPIER_HOOK_URL, {
+					phone: phoneNumber,
+					message: outOfRangeMessage,
+				});
+			}
+	
+			next();
+		} catch (error) {
+			next(error); // Pass the error to the error handling middleware
+		}
+	}
 };
 
 module.exports = alertController;
--- src/server/models/userModel.js
+++ src/server/models/userModel.js
@@ -6,6 +6,7 @@ const UserSchema = new Schema(
     {
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
+        phoneNumber: { type: String, required: false },
     },
     { collection: "users" }
 );
