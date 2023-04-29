09a6ca8d80127b928f630af4f99f9837cd320b06
Karbutov00 <kirillkarbutov@gmail.com>
2023-04-29 15:05:45 -0400

    topic

--- src/client/components/ClusterDynamicDetails.jsx
+++ src/client/components/ClusterDynamicDetails.jsx
@@ -1,66 +1,63 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import ClusterOverview from './ClusterOverview';
-import DynamicOverview from './DynamicOverview';
-import DynamicHealth from './DynamicHealth';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ClusterOverview from "./ClusterOverview";
+import DynamicOverview from "./DynamicOverview";
+import DynamicHealth from "./DynamicHealth";
+import DynamicTopics from "./DynamicTopics";
+import { useParams } from "react-router-dom";
 
 const ClusterDynamicDetails = ({ currentTab }) => {
-  const [metrics, setMetrics] = useState(0);
-  const [intervalId, setIntervalId] = useState(null);
-  const [clusterName, setClusterName] = useState('');
-  const [cluster, setCluster] = useState({});
-  const params = useParams();
-  useEffect(() => {
-    axios
-      .get(`http://localhost:3001/cluster/clusterById/${params.id}`)
-      .then((res) => {
-        setCluster(res.data);
-        console.log(res.data);
-      });
-  }, [clusterName]);
+    const [metrics, setMetrics] = useState(0);
+    const [intervalId, setIntervalId] = useState(null);
+    const [clusterName, setClusterName] = useState("");
+    const [cluster, setCluster] = useState({});
+    const params = useParams();
+    useEffect(() => {
+        axios
+            .get(`http://localhost:3001/cluster/clusterById/${params.id}`)
+            .then((res) => {
+                setCluster(res.data);
+                console.log(res.data);
+            });
+    }, [clusterName]);
 
-  switch (currentTab) {
-    case 'overview':
-      return (
-        <DynamicOverview
-          cluster={cluster}
-          setIntervalId={setIntervalId}
-          intervalId={intervalId}
-        />
-      );
-    /* 
+    switch (currentTab) {
+        case "overview":
+            return (
+                <DynamicOverview
+                    cluster={cluster}
+                    setIntervalId={setIntervalId}
+                    intervalId={intervalId}
+                />
+            );
+        /* 
             cluster,
             setUpdatingCluster,
             clusterId,
             setCluster,
             setIntervalId,
             */
-    case 'health':
-      return (
-        <DynamicHealth
-          cluster={cluster}
-          setIntervalId={setIntervalId}
-          intervalId={intervalId}
-        />
-      );
+        case "health":
+            return (
+                <DynamicHealth
+                    cluster={cluster}
+                    setIntervalId={setIntervalId}
+                    intervalId={intervalId}
+                />
+            );
 
-    case 'topic':
-      return (
-        <div>
-          <img
-            src='https://paradepets.com/.image/t_share/MTkxMzY1Nzg4Njc2NzI4NDE4/golden-retriever.jpg'
-            alt=''
-          />
-        </div>
-      );
-    default: 
-        return (
-          <div>default</div>
-        )
-
-  }
+        case "topic":
+            return (
+                <DynamicTopics
+                    cluster={cluster}
+                    setIntervalId={setIntervalId}
+                    intervalId={intervalId}
+                />
+            );
+        default:
+            return <div>default</div>;
+    }
 };
 
 export default ClusterDynamicDetails;
