3ef2fdaa972bb01a0e731e8a186db1f75250463d
Sidney Brodsky <sidneybrodsky@Sidneys-MacBook-Air.local>
2023-05-01 15:06:53 -0400

    more

--- src/client/components/DynamicTopics.jsx
+++ src/client/components/DynamicTopics.jsx
@@ -6,151 +6,121 @@ import {
     AccordionDetails,
     AccordionSummary,
     Box,
-} from "@mui/material";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import React, { useState, useEffect } from "react";
-import TopicDetail from "./TopicDetail";
-import axios from "axios";
-const DynamicTopics = ({ cluster }) => {
+  } from '@mui/material';
+  import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+  import React, { useState, useEffect } from 'react';
+  import TopicDetail from './TopicDetail';
+  import axios from 'axios';
+  const DynamicTopics = ({ cluster }) => {
     const [expanded, setExpanded] = useState(false);
     const handleChange = (panel) => (event, isExpanded) => {
-        setExpanded(isExpanded ? panel : false);
+      setExpanded(isExpanded ? panel : false);
     };
-    useEffect(() => {
-        axios
-            .post("http://localhost:3001/jmx/topics", {
-                broker: cluster.prometheusUrl,
-            })
-            .then((response) => console.log(response.data))
-            .catch((e) => console.log(e));
-    }, []);
     const [topics, setTopics] = useState({
-        resultType: "vector",
-        result: [
-            {
-                metric: {
-                    topic: "__consumer_offsets",
-                },
-                value: [1682792028.564, "50"],
-            },
-            {
-                metric: {
-                    topic: "purchases",
-                },
-                value: [1682792028.564, "1"],
-            },
-            {
-                metric: {
-                    topic: "purchases2",
-                },
-                value: [1682792028.564, "1"],
-            },
-            {
-                metric: {
-                    topic: "quickstart-events",
-                },
-                value: [1682792028.564, "1"],
-            },
-            {
-                metric: {
-                    topic: "sales",
-                },
-                value: [1682792028.564, "5"],
-            },
-        ],
+      resultType: 'vector',
+      result: [
+        {
+          metric: {
+            topic: '__consumer_offsets',
+          },
+          value: [1682963429.198, '50'],
+        },
+      ],
     });
-
-    const [searchText, setSearchText] = useState("");
-    console.log(searchText);
+  
+    useEffect(() => {
+      axios
+        .post('http://localhost:3001/jmx/topics', {
+          broker: cluster.prometheusUrl,
+        })
+        .then((response) => {
+          setTopics(response.data);
+          console.log(response.data);
+        })
+        .catch((e) => console.log(e));
+    }, []);
+    const [searchText, setSearchText] = useState('');
     return (
-        <Paper
-            sx={{
-                width: "90%",
-                height: "95%",
-                marginLeft: "175px",
-                marginTop: "55px",
-                textAlign: "center",
-                padding: "20px",
-            }}
+      <Paper
+        sx={{
+          width: '90%',
+          height: '95%',
+          marginLeft: '175px',
+          marginTop: '55px',
+          textAlign: 'center',
+          padding: '20px',
+        }}
+      >
+        <Typography component='header' variant='h4' sx={{ padding: '10px' }}>
+          {' '}
+          Search Topic
+        </Typography>
+        <TextField
+          placeholder='Search'
+          sx={{ width: '70%' }}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            width: '75%',
+            alignItems: 'center',
+            marginLeft: '175px',
+            mt: '20px',
+          }}
         >
-            <Typography
-                component="header"
-                variant="h4"
-                sx={{ padding: "10px" }}
-            >
-                {" "}
-                Search Topic
-            </Typography>
-            <TextField
-                placeholder="Search"
-                sx={{ width: "70%" }}
-                onChange={(e) => setSearchText(e.target.value)}
-            />
-            <Box
-                sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    width: "75%",
-                    alignItems: "center",
-                    marginLeft: "175px",
-                    mt: "20px",
-                }}
-            >
-                {topics?.result
-                    .filter((value) => {
-                        if (searchText === "") {
-                            console.log(value);
-                            return value;
-                        } else if (
-                            value.metric.topic
-                                .toLowerCase()
-                                .includes(searchText.toLowerCase())
-                        ) {
-                            console.log(value);
-                            return value;
-                        }
-                    })
-                    .map((topic, index) => {
-                        return (
-                            <Accordion
-                                expanded={
-                                    expanded ===
-                                    `${topic?.metric.topic}${index}`
-                                }
-                                onChange={handleChange(
-                                    `${topic?.metric.topic}${index}`
-                                )}
-                                key={index}
-                                sx={{
-                                    width: "90%",
-                                    alignItems: "center",
-                                    justifyContent: "space-between",
-                                    margin: "5px",
-                                }}
-                            >
-                                <AccordionSummary
-                                    expandIcon={<ExpandMoreIcon />}
-                                    aria-controls={`${topic?.metric.topic}${index}-content`}
-                                >
-                                    <Typography
-                                        sx={{
-                                            width: "33%",
-                                            flexShrink: 0,
-                                            fontWeight: "bold",
-                                        }}
-                                    >
-                                        {topic?.metric.topic}
-                                    </Typography>
-                                </AccordionSummary>
-                                <AccordionDetails>
-                                    <TopicDetail topic={topic.metric.topic} />
-                                </AccordionDetails>
-                            </Accordion>
-                        );
-                    })}
-            </Box>
-        </Paper>
+          {topics.data?.result
+            .filter((value) => {
+              if (searchText === '') {
+                console.log(value);
+                return value;
+              } else if (
+                value.metric.topic
+                  .toLowerCase()
+                  .includes(searchText.toLowerCase())
+              ) {
+                console.log(value);
+                return value;
+              }
+            })
+            .map((topic, index) => {
+              return (
+                <Accordion
+                  expanded={expanded === `${topic?.metric.topic}${index}`}
+                  onChange={handleChange(`${topic?.metric.topic}${index}`)}
+                  key={index}
+                  sx={{
+                    width: '90%',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    margin: '5px',
+                  }}
+                >
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls={`${topic?.metric.topic}${index}-content`}
+                  >
+                    <Typography
+                      sx={{
+                        width: '33%',
+                        flexShrink: 0,
+                        fontWeight: 'bold',
+                      }}
+                    >
+                      {topic?.metric.topic}
+                    </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <TopicDetail topic={topic?.metric.topic} cluster={cluster} />
+                  </AccordionDetails>
+                </Accordion>
+              );
+            })}
+        </Box>
+      </Paper>
     );
-};
-
-export default DynamicTopics;
+  };
+  
+  export default DynamicTopics;
+  
--- src/server/controllers/clusterController.js
+++ src/server/controllers/clusterController.js
@@ -14,6 +14,7 @@ const clusterController = {
             return next();
         } catch (error) {
             console.log(error);
+            return next(error);
         }
     },
 
