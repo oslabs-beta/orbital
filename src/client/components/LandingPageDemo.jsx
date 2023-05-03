7ea79aa5b25fc1de96dea2162abf4e4f073c3c52
Victor Gulyak <victg932@gmail.com>
2023-05-03 15:51:56 -0400

    fixed landing page

--- src/client/components/LandingPageDemo.jsx
+++ src/client/components/LandingPageDemo.jsx
@@ -1,5 +1,7 @@
-import { Box, Container, Grid, Typography, Button } from "@mui/material";
-// import demoGif from "../assets/demo.gif";
+import { Box, Container, Grid, Typography, Divider } from "@mui/material";
+import Cluster from "../assets/Cluster.gif";
+import Details from "../assets/More-Details.gif";
+import topics from "../assets/topics.gif";
 
 const DemoSection = () => {
 
@@ -21,7 +23,7 @@ const DemoSection = () => {
                   Demo
                 </Typography>
                 <Typography variant="subtitle1" gutterBottom>
-                See our quick demo to learn how to use Orbital to monitor your Apache Kafka clusters.
+                  See our quick demo to learn how to use Orbital to monitor your Apache Kafka clusters.
                 </Typography>
                 <Box sx={{ mt: { xs: 2, md: 4 } }}>
              
@@ -29,7 +31,10 @@ const DemoSection = () => {
               </Grid>
               <Grid item xs={12} md={6} sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
                 <Box sx={{ maxWidth: "100%", maxHeight: "100%" }}>
-                <img src="https://media.tenor.com/9XiRPDIVL-EAAAAd/trashcan-animation-random.gif" alt="Demo Gif Here" style={{ height: "auto", maxWidth: "100%", objectFit: "contain" }} />
+                <Divider sx={{ width: '80%', margin: 'auto', marginTop: 4, marginBottom: 4 }} />
+                  <img src={Details} alt="Demo Gif Here" style={{ height: "auto", maxWidth: "100%", objectFit: "contain" }} />
+                  <img src={topics} alt="Demo Gif Here" style={{ height: "auto", maxWidth: "100%", objectFit: "contain" }} />
+                  <Divider sx={{ width: '80%', margin: 'auto', marginTop: 4, marginBottom: 4 }} />
                 </Box>
               </Grid>
             </Grid>
